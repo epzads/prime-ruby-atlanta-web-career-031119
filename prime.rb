4d1243aa64d9a98a4540bdeac6106ac7e3176b29
favorable-vector-1743 <epzads@gmail.com>
2019-03-03 17:12:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,4 +7,19 @@ def prime?(number)
     
   else 
     
+    i =  2 
     
+    while i < number 
+    
+    if number % i == 0 
+      
+      return false 
+     end 
+      
+      i += 1 
+      
+      
+    end 
+    
+    
+      
